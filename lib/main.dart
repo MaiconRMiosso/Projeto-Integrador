690b8b7ed8f6ec70ba41a3ced6c2decf84d15d5a
Caroline Kolassa <carolinekolassa@gmail.com>
2022-09-08 21:39:19 -0300

    alteracoes

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:flutter/services.dart';
 import 'package:oi/LocalizacaoAluno.dart';
 import 'package:oi/cadastroAluno.dart';
 import 'package:oi/cadastrodemensagem.dart';
-import 'package:oi/home.dart';
+import 'package:oi/homeAluno.dart';
 import 'package:oi/homeMotorista.dart';
 import 'package:oi/homeAluno.dart';
+import 'package:oi/listaNotificacaoAluno.dart';
 import 'package:oi/listaaluno.dart';
 import 'package:oi/listaembarques.dart';
-import 'package:oi/listalonotificacao.dart';
 import 'package:oi/notificacaoAluno.dart';
 import 'cadastroTransporte.dart';
 import 'login.dart';
@@ -17,7 +17,7 @@ import 'cadastroTransporte.dart';
 void main() {
   runApp(MaterialApp(
 
-    home: login(), //trocar aqui conforme os componentes que querem usar
+    home: listaNotificacaoAluno(), //trocar aqui conforme os componentes que querem usar
 
     debugShowCheckedModeBanner: false,
   ));
